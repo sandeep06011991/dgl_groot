79469b9aa5bc548be7e6fea808d48ccc2745386d
sandeep06011991 <sandeep06011991@gmail.com>
2023-11-02 19:50:46 +0000

    minor remove duplciates

---
+++ python/util_exp.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def clean_and_sort(filename):
+    df = pd.read_csv(filename)
+
+    is_duplicate = df.duplicated(subset=['machine_name', 'graph_name', 'world_size', 'num_epoch', 'fanouts',
+                                         'num_redundant_layers', 'batch_size', 'system', 'model', 'hid_size',
+                                         'cache_rate', 'partition_type'], keep = 'last')
+
+    unique_df = df[~ is_duplicate]
+
+    sorted_df = unique_df.sort_values(['machine_name', 'graph_name', 'world_size', 'num_epoch', 'fanouts',
+                                   'batch_size','model', 'hid_size',
+                                   'system', 'num_redundant_layers', 'cache_rate', 'partition_type'])
+    with open(filename,'w') as fp:
+        fp.write(sorted_df.to_csv(index = False))
+    print("removed", is_duplicate.sum(), "from", filename)
+
+if __name__ == "__main__":
+    filenames= ["batch_size", "depth", "partitions", \
+        "default", "hidden_size", "redundant_layers"]
+
+    for filename in filenames:
+        clean_and_sort(f'log/{filename}.csv')
